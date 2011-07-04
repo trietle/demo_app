0af117aa0a2cdc80763986f60fef0506c4df0ccc
John Le <trietminhle09@gmail.com>
2011-07-05 00:16:02 +0700

    set root_path

--- config/routes.rb
+++ config/routes.rb
@@ -52,7 +52,7 @@ DemoApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "users#index"
 
   # See how all your routes lay out with "rake routes"
 
@@ -60,3 +60,4 @@ DemoApp::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
 end
+
